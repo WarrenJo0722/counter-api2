1377ffb236c2de101185c7d948b06d348e743d72
keonjo <warrenjo0722@gmail.com>
2026-02-13 15:43:51 +0900

    deploy2

--- src/main/java/com/example/counter/service/CounterService.java
+++ src/main/java/com/example/counter/service/CounterService.java
@@ -1,10 +1,13 @@
 package com.example.counter.service;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Service;
 
 @Service
 public class CounterService {
+    private static final Logger log = LoggerFactory.getLogger(CounterService.class);
     private final StringRedisTemplate redisTemplate;
     private static final String COUNTER_KEY = "visit;count";
 
@@ -13,6 +16,7 @@ public class CounterService {
     }
 
     public Long increment() {
+        log.info("sada");
         return redisTemplate.opsForValue().increment(COUNTER_KEY);
     }
 
